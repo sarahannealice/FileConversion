e64f158c5fe29a18d1e5de3e20a2cc0083f20d64
sarah <sarah.annealice@gmail.com>
2023-01-27 15:26:10 -0400

    asked for user input in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
@@ -9,24 +10,40 @@ int main() {
 
     //---TO DO---//
     //1. ask for user input (file path to read)
-    //2. validate user input
+    //2. validate user input (check for whitespaces)
     //3. read file content
     //4. replace < > chevrons with &lt; &gt; respectively
     //5. add <PRE> and </PRE> to the beginning and end of the file respectively
     //6. write to new file with .html extension
 
-    string myLine;
+    //variables
+    string filePath;
+    string fileDest;
+    char checkChar;
 
-    ifstream myFile (R"(C:\Users\sarah\Desktop\sarah\nscc\winter 2023\PROG2100\test.txt)");
+    cout << "Input the absolute file path for the desired file to be converted: ";
+    cin >> fileDest;
 
-    if (myFile.is_open()) {//checks if file is open
-        while (myFile) {//while loop to read entire file -- equivalent to myFile.good()
-            getline(myFile, myLine);//pipes content into stream
-            cout << myLine << endl;//pipes content to standard output
-        }
-    } else {//to catch if the file can't be opened
-        cout << myLine << "couldn't open file\n";
-    }
+
+
+
+
+
+
+
+//****************TESTING FILE IO****************//
+//    string myLine;
+//
+//    ifstream myFile (R"(C:\Users\sarah\Desktop\sarah\nscc\winter2023\PROG2100\test.txt)");
+//
+//    if (myFile.is_open()) {//checks if file is open
+//        while (myFile) {//while loop to read entire file -- equivalent to myFile.good()
+//            getline(myFile, myLine);//pipes content into stream
+//            cout << myLine << endl;//pipes content to standard output
+//        }
+//    } else {//to catch if the file can't be opened
+//        cout << myLine << "couldn't open file\n";
+//    }
 
     return 0;
 }
